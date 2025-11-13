e5aec1323ba56f7018403ac3c0f778522dfbbfee
yaadevmohit <mohityadav530@gmail.com>
2025-11-13 12:55:07 -0500

    fixing img

---
+++ src/assets/profileData.tsx
@@ -0,0 +1,11 @@
+import profileImg from "./IMG_1368.JPG";
+export default {
+    name: "Mohit Yadav",
+    title: ["Web Developer", "React Expert", "Creative Coder"],
+    description: "A passionate software developer specializing in building exceptional digital experiences. Currently, I'm focused on developing responsive full-stack web applications.",
+    img: profileImg,
+    socialLinks: {
+        linkedIn: "https://www.linkedin.com/in/mohit-yadav-1234/",
+        github: ""
+    }
+}
