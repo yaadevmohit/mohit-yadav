6460a92e0b5d93641045cdf7457e313b1df92285
yaadevmohit <mohityadav530@gmail.com>
2026-01-26 17:02:01 -0500

    add footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="w-full text-center text-lg text-[#003566] nunitoRegular flex flex-col sm:absolute left-0 bottom-5">
+        <footer className="w-full text-center text-lg text-[#003566] nunitoRegular flex flex-col lg:absolute left-0 bottom-5">
             <ul className="flex mx-auto space-x-3">
-              <li className="my-0 underline hover:scale-110"><a href="">github</a></li>
-              <li className="my-0 underline hover:scale-110"><a href="">email me</a></li>
+              <li className="my-0 underline hover:scale-110"><a href="https://github.com/yaadevmohit" target="_blank">github</a></li>
+              <li className="my-0 underline hover:scale-110"><a href="mailto:mohityadav530@gmail.com">email me</a></li>
             </ul>
             © {new Date().getFullYear()} Mohit Yadav. All rights reserved.
         </footer>
