38afe974547055711bf11bcce3cde1e4b2894d9f
yaadevmohit <mohityadav530@gmail.com>
2026-03-04 12:47:11 -0500

    description updated

--- src/assets/projectsData.tsx
+++ src/assets/projectsData.tsx
@@ -1,20 +1,11 @@
-// import libraryImg from "../projects_img/pr-1.png"
-// import formImg from "../projects_img/pr-2.png"
+import formImg from "./modernForm.png"
 export default 
     [
         {
-            name: "Library",
-            // img: libraryImg,
-            info: "Library app which dynamically searches for entered inputs and finds the correct title, author name and cover image.",
-            skills: ["React", "JavaScript", "HTML", "CSS"],
-            github: "https://github.com/yaadevmohit/library",
-            live: "https://library-by-mohit.vercel.app/"
-        },
-        {
             name: "Modern Form",
-            // img: formImg,
-            info: "Four steps form done as part of frontendmentor challanges. Uses prop drilling to pass data from all the different states of the form.",
-            skills: ["React", "JavaScript", "HTML", "CSS"],
+            img: formImg,
+            description: "Four steps form done as part of frontendmentor challanges. Uses prop drilling to pass data from all the different states of the form.",
+            skills: ["React", "Typescript", "HTML", "CSS"],
             github: "https://github.com/yaadevmohit/modern-form",
             live: "https://multi-step-form-mohit.vercel.app/"
         }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+
+
 const Hero = () => {
   return (
     <main className="w-full md:my-12 my-3 flex flex-col lg:flex-row items-center gap-4 md:p-4 border-[#003566] md:border-8 rounded-2xl">
@@ -12,15 +14,13 @@ const Hero = () => {
             <h2
               className="text-2xl text-red-600 text-left md:text-3xl w-full mb-2 nunitoBold" 
             >
-             Web Developer based in Toronto  
+             Data Engineer based in Toronto  
             </h2>
             <hr />
             <p 
               className="text-lg  text-left md:mx-2 md:my-4 p-4 customYellowBgRing nunitoRegular"
             >
-              A web developer passionate about crafting fast, accessible, and visually engaging digital experiences.
-              I specialize in building responsive full-stack web applications using modern technologies like React, Node.js, and Tailwind CSS.
-              I love turning ideas into interactive, meaningful solutions that make an impact.
+            Data Integration Specialist with 5 years of experience designing and implementing robust ETL/ELT pipelines. Proficient in optimizing data workflows, ensuring data integrity, and managing high-volume migrations across cloud and on-premises environments. Proven track record of reducing processing times and collaborating with cross-functional teams to deliver clean, structured data for business intelligence and advanced analytics.
             </p>
         </div>
     </main>
